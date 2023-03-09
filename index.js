219a313aff929a8a7079e39186523b31d7256b01
Mrat Frezghi <mratfrezghi02@gmail.com>
2023-03-09 20:17:12 +0000

    Start

---
+++ index.js
@@ -0,0 +1,226 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require('fs');
+const { createCanvas, loadImage } = require("canvas");
+
+app.listen(4000, process.env.IP, function(){console.log(`Server is running on port 4000`);});
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+app.use("/Temp/", express.static("./Temp"));
+
+//functions------------------------------------------------------------------------
+var Animals = ReadAnimals();
+function ReadAnimals() {
+    const lines = fs.readFileSync('Animals.txt', 'utf-8').split(/\r?\n/);
+    return lines;
+}
+
+function ReadTemp(){
+  var files = []
+  fs.readdirSync('./Temp/').forEach(file => {
+    let info = {
+      filename: file,
+      fileloc: './Temp/' + file,
+    };
+    files.push(info)
+  });
+  return files;
+}
+
+function DeleteTemp(){
+  fs.readdir('./Temp/', (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join('./Temp/', file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+}
+//storage--------------------------------------------------------------------------
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'Temp')
+  },
+  filename: (req, file, cb) => {
+    //console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({storage: storage})
+
+//Database-------------------------------------------------------------------------
+const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database("./Database.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to Database.");
+});
+
+function DeleteInfo() {
+  db.run("DELETE FROM Image",function (err) {
+    if (err){return console.log(err.message);}
+  }); 
+  db.run("DELETE FROM Label",function (err) {
+    if (err){return console.log(err.message);}
+  }); 
+}
+
+
+//Pages----------------------------------------------------------------------------
+app.get("/", (req, res) => { 
+  DeleteTemp();
+  res.render("MainPage");
+});
+
+app.get("/About", (req, res) => { 
+  DeleteTemp();
+  res.render("About");
+});
+
+app.get("/Archive", (req, res) => { 
+  DeleteTemp();
+  db.all("SELECT * FROM Images", (err, rows) => {
+    if (err) {console.error(err.message);}
+    const imageFormat = "jpg";
+  })
+  res.render("Archive");
+});
+
+app.get("/comments", (req, res) => { 
+  DeleteTemp();
+  res.render("comments");
+});
+
+var orgiinalfilelength = 0;
+app.post("/upload", upload.array("filetoupload") ,(req, res) => { 
+  var files = ReadTemp();
+
+  files.forEach((file) => {
+    localizeObjects(file);
+  });
+  
+  files.forEach((file) => {
+    labelDetection(file)
+  });
+
+  console.log(files.length)
+  orgiinalfilelength = files.length;
+  res.render("upload");
+});
+
+app.get("/Results", (req, res) => { 
+  var filesdif = ReadTemp();
+  showfiles = [];
+  filesdif.forEach((file) => {
+    fname = file.filename;
+    if (fname.includes("Box")){
+      showfiles.push(file.fileloc)
+    }
+  });
+
+  newfiles = filesdif.length;
+  diffiles = newfiles - orgiinalfilelength
+  wrongfiles = Math.abs(diffiles - orgiinalfilelength)
+  console.log(wrongfiles)
+  console.log(filesdif.length)
+
+  var message = null;
+  if(wrongfiles != 0){
+    message = "There where "+wrongfiles+" Image that did not contain Animals";
+  }
+  console.log(showfiles)
+  res.render("Results", { Boximage: showfiles, errormes: message});
+});
+//API------------------------------------------------------------------------------
+const vision = require("@google-cloud/vision");
+
+const client = new vision.ImageAnnotatorClient({ keyFilename: "key.json" });
+
+async function localizeObjects(file) {
+  const request = { image: { content: fs.readFileSync(file.fileloc) } };
+
+  const [result] = await client.objectLocalization(request);
+  const objects = result.localizedObjectAnnotations;
+
+  var objectlable = [];
+
+  var objectdetails = [];
+
+  objects.forEach((object) => {
+    coordinates = [];
+    const vertices = object.boundingPoly.normalizedVertices;
+
+    vertices.forEach((v) => coordinates.push(v.x * 1, v.y * 1));
+
+    objectlable.push(object.name);
+
+    let cobject = {name: object.name, coord1x: coordinates[0], coord1y: coordinates[1], coord2x: coordinates[2], coord2y: coordinates[3], coord3x: coordinates[4], coord3y: coordinates[5], coord4x: coordinates[6], coord4y: coordinates[7],};
+
+    objectdetails.push(cobject);
+  });
+
+  found = objectlable.some((r) => Animals.indexOf(r) >= 0);
+  if (found === false) {
+    return;
+  }
+
+  loadImage(file.fileloc).then((image) => {
+    const canvas = createCanvas(image.width, image.height);
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+
+    ctx.strokeStyle = "#ff0000";
+    ctx.lineWidth = 4;
+    ctx.font = "30px Arial";
+
+    objectdetails.forEach((object) => {
+      ctx.beginPath();
+      ctx.moveTo(object.coord1x * image.width, object.coord1y * image.height);
+      ctx.lineTo(object.coord2x * image.width, object.coord2y * image.height);
+      ctx.lineTo(object.coord3x * image.width, object.coord3y * image.height);
+      ctx.lineTo(object.coord4x * image.width, object.coord4y * image.height);
+      ctx.closePath();
+      ctx.strokeStyle = "red";
+      ctx.lineWidth = 5;
+      ctx.stroke();
+      ctx.font = "bold 40px Arial";
+      ctx.fillStyle = "red";
+      ctx.fillText(object.name, object.coord1x * image.width, object.coord1y * image.height - 10);
+    });
+
+    const out = fs.createWriteStream("Temp/Box" + file.filename);
+    const stream = canvas.createJPEGStream();
+    stream.pipe(out);
+  });
+}
+
+async function labelDetection(file) {
+
+  const [result] = await client.labelDetection(file.fileloc);
+  const labels = result.labelAnnotations;
+
+  var path = './Temp/Box' + file.filename;
+  if (!fs.existsSync(path)) {return}
+
+  labels.forEach((label) => {
+    db.run("INSERT INTO Label (Label, Confidence, ImageName) VALUES (?, ?, ?)",[label.description,Math.round(label.score * 100),file.filename])
+  })
+
+  const simage = fs.readFileSync(file.fileloc);
+  const simageBuffer = Buffer.from(simage, "binary");
+  const sobjectloc = './Temp/Box' + file.filename;
+  const simageo = fs.readFileSync(sobjectloc);
+  const simageBuffero = Buffer.from(simageo, "binary");
+  db.run("INSERT INTO Image (Image, ImageObject, ImageName) VALUES (?, ?, ?)",[simageBuffer,simageBuffero,file.filename],function (err) {
+    if (err){return console.log(err.message);}
+  });  
+}
